213b4158e42aa42fcca25ba90f9c38e8a189fb46
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-02-16 13:22:34 +0700

    Change description to nullable

--- database/migrations/2020_02_07_131836_create_course_sublevels_table.php
+++ database/migrations/2020_02_07_131836_create_course_sublevels_table.php
@@ -19,7 +19,7 @@ class CreateCourseSublevelsTable extends Migration
             $table->string('title');
             $table->integer('time');
             $table->float('minimum_score');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
