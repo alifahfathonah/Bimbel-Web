84584789334548dccf0b5e4021219e863122f7d0
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-02-16 13:23:51 +0700

    Added custom input tag slot

--- resources/views/components/form_input.blade.php
+++ resources/views/components/form_input.blade.php
@@ -2,16 +2,19 @@
     <label for="{{ $name }}" class="col-sm-3 col-form-label text-right">
         {{ $label }}
     </label>
-
     <div class="col-sm-9">
+    @empty($input)
         <input type="{{ $type ?? 'text' }}"
-               class="form-control @error($name) is-invalid @enderror {{ $class ?? '' }}"
-               id="{{ $name }}"
-               name="{{ $name }}"
-               placeholder="{{ $label }}"
-               value="{{ old($name) ?? $value ?? '' }}"
-               {{ $prop ?? ''}}
-               >
+            class="form-control @error($name) is-invalid @enderror {{ $class ?? '' }}"
+            id="{{ $name }}"
+            name="{{ $name }}"
+            placeholder="{{ $label }}"
+            value="{{ old($name) ?? $value ?? '' }}"
+            {{ $prop ?? ''}}
+            >
+    @else
+        {{ $input }}
+    @endempty
 
         @error($name)
             <span class="invalid-feedback" role="alert">
