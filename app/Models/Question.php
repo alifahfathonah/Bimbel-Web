1b40122d177a4402aea6173c6a6638b863ecacc0
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-02-17 08:44:32 +0700

    Add fillable field

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = ['course_sublevel_id', 'question', 'number', 'type', 'media'];
+
     public function marked()
     {
         return $this->hasOne(MarkedQuestion::class, 'number', 'number');
+    }
+
+    public function choices()
+    {
+        return $this->hasMany(MultipleChoiceAnswer::class);
     }
 }
